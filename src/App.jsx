5c76308e92a83902b2351b11be9515e253d05d30
Daniel Filho <danbsilva98@gmail.com>
2021-10-01 09:20:27 -0300

    feat: add initial settings
    Add template with commitlint and husky, fonts and colors

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <main className="container">
+            <h1>#todo</h1>
+        </main>
+    );
+};
+
+export default App;
